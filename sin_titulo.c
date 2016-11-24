12b88b02585705206284d2b337d1b11bab997e88
Tarjebuschaco <xlxdaviirsxlx@gmail.com>
2016-11-23 21:34:49 -0300

    Add files via upload

---
+++ sin_titulo.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int j,x,a,b;
+int main()
+{
+	for (j=2;j<=541;j++){
+	b=0;
+	
+	for (a=1;a<=j;a++){
+		if (j%a==0){
+		b++;
+		}
+	}
+	
+		if (b<=2){
+		printf ("%d\n", j);
+		
+		}
+	}
+}
